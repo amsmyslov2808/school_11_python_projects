462631cd460047c0e89fab4fa22d5d2b10aa9c90
amsmyslov <a.m.smyslov@yandex.ru>
2026-07-21 20:45:13 +0300

    done menu

--- lesson20_dataclasses_2/main.py
+++ lesson20_dataclasses_2/main.py
@@ -14,7 +14,27 @@ while is_run == True:
     print("*" * 20)
 
     print("Меню:")
+    print("1. Вывести пост по ИД")
+    print("2. Добавить новый пост")
+    print("3. Обновить пост по ИД")
+    print("4. Удалить пост по ИД")
+    print("0. Выйти из программы")
 
+    choose_action = int(input("Введите номер выбранного пункта меню: "))
+
+    if choose_action == 1:
+        pass
+    elif choose_action == 2:
+        pass
+    elif choose_action == 3:
+        pass
+    elif choose_action == 4:
+        pass
+    elif choose_action == 0:
+        pass
+
+    print("=" * 50)
+    input("Для продолжения нажмите <Enter>")
 
 # new_post = input_post_data()
 # new_post.id = get_next_post_id()
