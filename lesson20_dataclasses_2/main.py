ede68781ec2d996730ce3a0ebb12948e0e191e76
amsmyslov <a.m.smyslov@yandex.ru>
2026-07-21 20:56:18 +0300

    done 1 menu point

--- lesson20_dataclasses_2/main.py
+++ lesson20_dataclasses_2/main.py
@@ -3,6 +3,47 @@ from posts_functions import *
 
 posts: list[Post] = []
 
+post_1 = Post(
+    id=get_next_post_id(),
+    posting_datetime="2026-07-21T14:30:00Z",
+    media_url="https://example.com",
+    description="Прекрасный закат на побережье! #отпуск #море",
+)
+
+post_2 = Post(
+    id=get_next_post_id(),
+    posting_datetime="2026-07-21T20:00:00Z",
+    media_url="https://example.com",
+    description="Пошаговый рецепт идеальной итальянской пасты за 15 минут.",
+)
+
+post_3 = Post(
+    id=get_next_post_id(),
+    posting_datetime="2026-07-22T09:00:00Z",
+    media_url="https://example.com",
+    description="Утреннее кардио: 10-минутная зарядка для бодрости на весь день. Комплекс упражнений внутри.",
+)
+
+post_4 = Post(
+    id=get_next_post_id(),
+    posting_datetime="2026-07-22T14:30:00Z",
+    media_url="https://example.com",
+    description="Топ-5 книг по личной эффективности, которые изменят ваше отношение к тайм-менеджменту.",
+)
+
+post_5 = Post(
+    id=get_next_post_id(),
+    posting_datetime="2026-07-23T18:15:00Z",
+    media_url="https://example.com",
+    description="Итоги нашей поездки в горы: честный отзыв об отелях, маршрутах и скрытых локациях.",
+)
+
+add_new_post_to_end_of_list(posts, post_1)
+add_new_post_to_end_of_list(posts, post_2)
+add_new_post_to_end_of_list(posts, post_3)
+add_new_post_to_end_of_list(posts, post_4)
+add_new_post_to_end_of_list(posts, post_5)
+
 is_run = True
 
 while is_run == True:
@@ -23,7 +64,16 @@ while is_run == True:
     choose_action = int(input("Введите номер выбранного пункта меню: "))
 
     if choose_action == 1:
-        pass
+        id = int(input("Введите ИД поста: "))
+
+        found_post = get_post_by_id(posts, id)
+
+        if found_post != None:
+            print_post_header()
+            print_one_post(found_post)
+        else:
+            print("Пост не найден")
+
     elif choose_action == 2:
         pass
     elif choose_action == 3:
@@ -31,37 +81,7 @@ while is_run == True:
     elif choose_action == 4:
         pass
     elif choose_action == 0:
-        pass
+        is_run = False
 
     print("=" * 50)
     input("Для продолжения нажмите <Enter>")
-
-# new_post = input_post_data()
-# new_post.id = get_next_post_id()
-
-# add_new_post_to_end_of_list(posts, new_post)
-
-# new_post = input_post_data()
-# new_post.id = get_next_post_id()
-
-# add_new_post_to_end_of_list(posts, new_post)
-
-post_1 = Post(
-    id=get_next_post_id(),
-    posting_datetime="2026-07-21T14:30:00Z",
-    media_url="https://example.com",
-    description="Прекрасный закат на побережье! #отпуск #море",
-)
-
-# Второй мок-объект (без id, видео)
-post_2 = Post(
-    id=get_next_post_id(),
-    posting_datetime="2026-07-21T20:00:00Z",
-    media_url="https://example.com",
-    description="Пошаговый рецепт идеальной итальянской пасты за 15 минут.",
-)
-
-add_new_post_to_end_of_list(posts, post_1)
-add_new_post_to_end_of_list(posts, post_2)
-
-print_posts(posts)
