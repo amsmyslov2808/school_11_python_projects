84f126e44f5529d38efc4863ee8ae877cee6de64
amsmyslov <a.m.smyslov@yandex.ru>
2026-07-23 21:22:02 +0300

    done zachet

---
+++ lesson18_zachet/sample17.py
@@ -0,0 +1,45 @@
+# "hello" = ['h','e','l','l','o']
+
+# text = "hello"  # "helo"
+
+# text[0] = "d"
+
+# print(text[:3] + text[-1:])
+
+# arr = ["h", "e", "l", "l", "o"]
+# arr[0] = "d"
+# print(arr)
+
+
+# def swap(a, b):
+#     # a, b = b, a
+#     temp = a
+#     a = b
+#     b = temp
+
+
+# a = 3
+# b = 5
+
+# print(f"a = {a} b = {b}")
+
+# swap(a, b)
+
+# print(f"a = {a} b = {b}")
+
+
+# def add_to_end(arr, val):
+#     arr.append(val)
+
+
+# arr = [1, 2, 3]
+
+# print(arr)
+
+# add_to_end(arr, 999)
+
+# print(arr)
+
+result = True
+
+answer = "da" if result else "net"
