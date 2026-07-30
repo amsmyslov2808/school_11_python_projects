4c617479f3f25b6a840c6b067ac87a0d61b37119
amsmyslov <a.m.smyslov@yandex.ru>
2026-07-30 20:14:27 +0300

    done structure

--- lesson21_big_project_1/products_functions.py
+++ lesson21_big_project_1/products_functions.py
@@ -16,6 +16,10 @@ def get_product_by_id(products: list[Product], search_id: int) -> Product | None
     pass
 
 
+def add_product_to_list(products: list[Product], product: Product):
+    pass
+
+
 def update_product_by_id(products: list[Product], product: Product) -> bool:
     pass
 
@@ -33,4 +37,30 @@ def print_single_product(product: Product):
 
 
 def print_all_products(products: list[Product]):
+    pass
+
+
+def sort_products_by_type_sort(products: list[Product], type_sort: int):
+    pass
+
+
+def find_products_by_type_parameter(
+    products: list[Product], type_parameter: int, parameter: str
+) -> list[Product]:
+    pass
+
+
+def buy_product(products: list[Product], search_id: int, request_amount: int) -> bool:
+    pass
+
+
+def load_products_from_txt_file(filename: str) -> list[Product]:
+    pass
+
+
+def save_products_to_txt_file(products: list[Product], filename: str) -> bool:
+    pass
+
+
+def save_products_to_txt_file_for_print(products: list[Product], filename: str) -> bool:
     pass
